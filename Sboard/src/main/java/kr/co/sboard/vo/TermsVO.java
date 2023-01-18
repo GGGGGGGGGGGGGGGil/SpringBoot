8e0914017fadbda36a2202a9001d488c7f0fcafb
JavApext <JavApext@gmail.com>
2023-01-18 13:57:59 +0900

    2023/01/18 스프링부트 게시판 시작

---
+++ Sboard/src/main/java/kr/co/sboard/vo/TermsVO.java
@@ -0,0 +1,12 @@
+package kr.co.sboard.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//생성자 필요없어서 겟, 셋만
+@Getter @Setter
+public class TermsVO {
+	
+	private String terms;
+	private String privacy;
+}
---
+++ Sboard/src/test/java/SBoard/SboardApplicationTests.java
@@ -0,0 +1,28 @@
+package SBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kr.co.sboard.repository.userRepo;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class SboardApplicationTests {
+
+	@Autowired
+	private userRepo repo;
+	
+	void contextLoads() {
+	}
+
+	@Test
+	public void countTest() {
+		
+		int result = repo.countByUid("123");
+		
+		log.info("result : "+result);
+	}
+	
+}
